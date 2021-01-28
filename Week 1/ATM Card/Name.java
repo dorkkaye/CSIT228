ca4c859ef5ba0869316138062ea16d93e666b5f2
Kaye Engbino <engbinop@gmail.com>
2021-01-28 18:56:28 +0800

    Name in ATM

---
+++ Week 1/ATM Card/Name.java
@@ -0,0 +1,50 @@
+public class Name 
+{
+    private String firstname;
+    private String middlename;
+    private String lastname;
+
+    public Name(){}
+
+    public Name(String firstname, String middlename, String lastname)
+    {
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = firstname;
+    }
+
+    public void setFirstName(String firstname)
+    {
+        this.firstname = firstname;
+    }
+
+    public void setMiddleName(String middlename)
+    {
+        this.middlename = middlename;
+    }
+
+    public void setLastName(String lastname)
+    {
+        this.lastname = lastname;
+    }
+
+    public String getFirstName()
+    {
+        return firstname;
+    }
+    
+    public String getMiddleName()
+    {
+        return middlename;
+    }
+    
+    public String getLastName()
+    {
+        return lastname;
+    }
+
+    public String toString()
+    {
+        return lastname + ", " + firstname + " " + middlename;
+    }
+}
